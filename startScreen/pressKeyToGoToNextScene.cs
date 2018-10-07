2339b28f2a13a05bc834b30538beb2ad3f39e43a
Tony Yi <24402057+tsmswifty@users.noreply.github.com>
2018-10-07 14:28:11 -0400

    start screen and other files

---
+++ startScreen/pressKeyToGoToNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pressKeyToGoToNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			SceneManager.LoadScene ("Intro Scene");
+		}
+	}
+}
